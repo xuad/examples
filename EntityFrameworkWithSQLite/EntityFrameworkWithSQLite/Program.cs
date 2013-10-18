61737aa525214bb405f0fe042fbb2f1103439add
Patrick Mosch <info@xuad.net>
2013-10-18 14:18:18 +0200

    Add SQLite tutorial projects

---
+++ EntityFrameworkWithSQLite/EntityFrameworkWithSQLite/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkWithSQLite
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var instance = new MovieDatabaseEntities())
+			{
+				// Clear table
+				instance.Database.ExecuteSqlCommand("DELETE FROM movies");
+
+				// Create new movie
+				Movies movie = new Movies();
+				movie.Name = "Kill Bill: Vol. 1";
+				movie.Director = "Quentin Tarantino";
+				movie.Playtime = 111;
+
+				// Add movie to database table
+				instance.Movies.Add(movie);
+
+				// Add more movies
+				instance.Movies.Add(new Movies("Pulp Fiction", "Quentin Tarantino", 1540));
+				instance.Movies.Add(new Movies("Reservoir Dogs", "Quentin Tarantino", 99));
+				instance.Movies.Add(new Movies("Wall-E", "Andrew Stanton", 98));
+				instance.Movies.Add(new Movies("Drive ", "Nicolas Winding Refn", 100));
+
+				// Save movies
+				instance.SaveChanges();
+
+				// Get a movie
+				Movies wantedMovie = instance.Movies.SingleOrDefault(item => item.Name == "Pulp Fiction");
+				wantedMovie.Playtime = 154;
+
+				// Save changed movie
+				instance.SaveChanges();
+
+				// Show changed movie
+				Console.WriteLine(string.Format("Filmname: {0}, Regisseur: {1}, Spielzeit: {2} min",
+					wantedMovie.Name,
+					wantedMovie.Director,
+					wantedMovie.Playtime.ToString()));
+
+				// Get all movies
+				List<Movies> allMovies = instance.Movies.ToList();
+
+				// Show all movies
+				foreach (Movies showMovie in allMovies)
+				{
+					Console.WriteLine(string.Format("Filmname: {0}, Regisseur: {1}, Spielzeit: {2} min",
+					showMovie.Name,
+					showMovie.Director,
+					showMovie.Playtime.ToString()));
+				}
+
+				// Wait for input
+				Console.ReadLine();
+			}
+		}
+	}
+}
---
+++ SimpleSQLiteExample/SimpleSQLiteExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSQLiteExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Create sqlite connection
+			SQLiteConnection connection = new SQLiteConnection(string.Format(@"Data Source={0}\SimpleDatabase.s3db", Environment.CurrentDirectory));
+
+			// Open sqlite connection
+			connection.Open();
+
+			// Get all rows from example_table
+			SQLiteDataAdapter db = new SQLiteDataAdapter("SELECT * FROM Names", connection);
+
+			// Create a dataset
+			DataSet ds = new DataSet();
+
+			// Fill dataset
+			db.Fill(ds);
+
+			// Create a datatable
+			DataTable dt = new DataTable("Names");
+			dt = ds.Tables[0];
+
+			// Close connection
+			connection.Close();
+
+			// Print table
+			foreach (DataRow row in dt.Rows)
+			{
+				Console.WriteLine(string.Format("{0} {1}", row["Firstname"], row["Surname"]));
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
